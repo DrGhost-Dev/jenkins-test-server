7cd04aec10ddef17dbc04edcb302908efc9c5415
DrGhost-Dev <jihwan2843@gmail.com>
2025-09-01 12:37:25 +0900

    test 3

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`aServer is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
