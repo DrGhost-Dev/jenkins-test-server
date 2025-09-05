5de9fdf72bf63ce9b9fbc4ba28a00a6da3b159ed
DrGhost-Dev <jihwan2843@gmail.com>
2025-09-05 13:09:12 +0900

    test 83

--- app/index.js
+++ app/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`abcdaServer is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
